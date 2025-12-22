c2e3b9349768363f0c2be9cbc52a4249337bdbcf
Walid <Walidbg3@gmail.com>
2025-12-22 15:24:22 +0100

    TD01 : Ajouter un composant Header dans la page

--- td01/src/App.jsx
+++ td01/src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mbdslogo from './assets/mbds.jpg'
 import './App.css'
 
+function Header() {
+  return (
+    <div>
+          <img src={mbdslogo} className="logo" alt="MBDS logo" />
+          <h1>Introduction à React</h1>
+          <h2>A la découverte des premières notions de React</h2>
+    </div>
+  );
+}
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Walid</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
     </>
   )
 }
